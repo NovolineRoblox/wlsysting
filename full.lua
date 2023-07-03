9abfd5d243dab6106464377fb342f55623ab04f5
Novoline4Roblox <125512934+StaryLOL@users.noreply.github.com>
2023-07-02 20:54:00 -0700

    Update full.lua

--- full.lua
+++ full.lua
@@ -1,6 +1,5 @@
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NovolineRoblox/wlsysting/main/mainstuff.lua", true))()
-wait(0.1)
 game:GetService("TextChatService").ChatInputBarConfiguration.TargetTextChannel:SendAsync(("Novoline"))
 
 
