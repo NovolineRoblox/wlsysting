ffc23f8cebe912fba38cfca3dc9dce34b64f4e6b
heckeroncrack <124959846+heckeroncrack@users.noreply.github.com>
2023-07-17 04:35:40 +0200

    Update full.lua

--- full.lua
+++ full.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NovolineRoblox/wlsysting/main/mainstuff.lua", true))()
+game:GetService("TextChatService").ChatInputBarConfiguration.TargetTextChannel:SendAsync(("e"))
